914b2c1d8a658271c8698840f16c4942c113dcb9
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2025-02-12 10:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 # Configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:new_password@localhost/ai"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SECRET_KEY'] = 'lucky'
+app.config['SECRET_KEY'] = ''
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'firstie369@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kghi lqdd bktq lbpw'
+app.config['MAIL_USERNAME'] = 'fl'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
